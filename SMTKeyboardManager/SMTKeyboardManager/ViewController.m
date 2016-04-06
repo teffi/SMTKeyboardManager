82c6c7047c3055e06ab34a3e3336513d6a4291fd
teffi <steffi.tan07@gmail.com>
2016-04-06 16:08:02 +0800

    Set adjustScrollviewInsets to NO

--- SMTKeyboardManager/SMTKeyboardManager/ViewController.m
+++ SMTKeyboardManager/SMTKeyboardManager/ViewController.m
@@ -20,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.automaticallyAdjustsScrollViewInsets = NO;
 }
 
 - (void)didReceiveMemoryWarning {
